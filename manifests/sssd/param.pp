9e2db5ea6ae737394701b9df15bcfd89188dc047
Simon Josi <simon.josi@swisstxt.ch>
2013-04-23 03:58:07 -0400

    sssd::param add support for true values

--- manifests/sssd/param.pp
+++ manifests/sssd/param.pp
@@ -2,9 +2,10 @@ define authconfig::sssd::param(
   $value,
   $section = 'domain/default'
 ) {
-  $changes = is_string($value) ? {
-    true  => "set target[. = '${section}']/${name} ${value}",
-    false => "rm target[. = '${section}']/${name}",
+  if is_string($value) or $value {
+    $changes = "set target[. = '${section}']/${name} ${value}"
+  } else {
+    $changes = "rm target[. = '${section}']/${name}"
   }
 
   augeas{"sssd_${section}_${name}":
